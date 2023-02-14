b2f4c67179bd7a448ba918c6f9709a467a4ca32e
Michael Gimara <michael.gimara@houzz.com>
2023-02-14 18:57:11 +0200

    Group account summary network calls together

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class AccountSummaryViewController: UIViewController {
     
-    // Request Models
-    var profile: Profile?
-    var accounts: [Account] = []
-    
     // View Models
     var headerViewModel = AccountSummaryHeaderView.ViewModel(welcomeMessage: "Welcome", name: "", date: Date())
     var accountCellViewModels: [AccountSummaryCell.ViewModel] = []
@@ -37,8 +33,7 @@ extension AccountSummaryViewController {
         setupNavigationBar()
         setupTableView()
         setupTableHeaderView()
-        fetchProfile()
-        fetchAccounts()
+        fetchData()
     }
     
     private func setupNavigationBar() {
@@ -69,8 +64,8 @@ extension AccountSummaryViewController {
         var size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
         size.width = UIScreen.main.bounds.width
         headerView.frame.size = size
-        
         tableView.tableHeaderView = headerView
+        headerView.configure(with: headerViewModel)
     }
 }
 
@@ -103,45 +98,46 @@ extension AccountSummaryViewController {
 
 // MARK: Networking
 extension AccountSummaryViewController {
-    private func fetchProfile() {
+    private func fetchData() {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
-                self.profile = profile
-                DispatchQueue.main.async {
-                    self.configureTableHeaderView(with: profile)
-                }
+                self.configureTableHeaderViewModel(with: profile)
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        fetchAccounts(forUserId: "1") { result in
+            switch result {
+            case .success(let accounts):
+                self.configureTableCellViewModels(with: accounts)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            self.headerView.configure(with: self.headerViewModel)
+            self.tableView.reloadData()
         }
     }
     
-    private func configureTableHeaderView(with profile: Profile) {
-        let vm = AccountSummaryHeaderView.ViewModel(
+    private func configureTableHeaderViewModel(with profile: Profile) {
+        headerViewModel = AccountSummaryHeaderView.ViewModel(
             welcomeMessage: "Good morning,",
             name: profile.firstName,
             date: Date()
         )
-        headerView.configure(with: vm)
     }
     
-    private func fetchAccounts() {
-        fetchAccounts(forUserId: "1") { result in
-            switch result {
-            case .success(let accounts):
-                self.accounts = accounts
-                self.configureTableCells(with: accounts)
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func configureTableCells(with accounts: [Account]) {
+    private func configureTableCellViewModels(with accounts: [Account]) {
         accountCellViewModels = accounts.map {
             AccountSummaryCell.ViewModel(
                 accountType: $0.type,
